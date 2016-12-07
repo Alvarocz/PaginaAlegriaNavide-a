9f801b730a7ea97420cc64681f3132e177a2f1ea
Alvarocz <alvaromaster2015@gmail.com>
2016-12-06 22:14:45 -0500

    fix embed youtube error

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -2,7 +2,8 @@ module VideosHelper
   def embed(url)
     url = url.to_s
     if url.include? "youtu"
-      youtube_id = url.split("=").last
+      youtube_id = url.split("/").last
+      puts youtube_id
       content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}", frameborder: "0")
     end
   end
